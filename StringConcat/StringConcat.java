65448df87374dfb735e4c9cbe288950fd3e237ed
DavJesse <jesseomolo@gmail.com>
2025-11-06 14:22:12 +0300

    feat: add StringConcat

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,5 +1,10 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
+        if (s1 == null) {
+            s1 = "";
+        } else if (s2 == null) {
+            s2 = "";
+        }
         return s1 + s2;
     }
 }

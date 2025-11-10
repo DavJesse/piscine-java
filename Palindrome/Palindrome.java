91fd616e451a74cce6ba7055dc6e0cded25adba5
DavJesse <jesseomolo@gmail.com>
2025-11-10 12:02:00 +0300

    feat: add Palindrome

---
+++ Palindrome/Palindrome.java
@@ -0,0 +1,19 @@
+public class Palindrome {
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        if (s == null) {
+            return false;
+        }
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}

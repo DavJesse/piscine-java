83d5776741e3d7469cbd05f2b6d65c694264db47
DavJesse <jesseomolo@gmail.com>
2025-11-17 16:51:25 +0300

    feat: add StarInheritance

---
+++ StarInheritance/Star.java
@@ -0,0 +1,17 @@
+package StarInheritance;
+public class Star extends CelestialObject {
+    private double magnitude;
+
+    public Star() {
+        super();
+        this.magnitude = 0.0;
+    }
+
+    public double getMagnitude() {
+        return magnitude;
+    }
+
+    public void setMagnitude(double magnitude) {
+        this.magnitude = magnitude;
+    }
+}

1a2d305d2b3492f7eef307fcd7da61de268561eb
DavJesse <jesseomolo@gmail.com>
2025-11-18 10:32:10 +0300

    feat: add ListContains

---
+++ ListContains/ListContains.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class ListContains {
+    public static boolean containsValue(List<Integer> list, Integer value) {
+        for (Integer element : list) {
+            if (element.equals(value)) {
+                return true;
+            }
+        }
+        return false;
+    } 
+}

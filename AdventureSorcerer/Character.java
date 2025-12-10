db2e5fe3868a2b3a70d3273bd354222aad5cb158
DavJesse <jesseomolo@gmail.com>
2025-12-10 11:21:39 +0300

    feat: add Adventure Sorcerer

--- AdventureSorcerer/Character.java
+++ AdventureSorcerer/Character.java
@@ -1,19 +1,48 @@
-// Assuming the original Character class structure
+import java.util.ArrayList;
+import java.util.List;
+
 public class Character {
+    // --- Existing Attributes ---
     protected String name;
     protected int maxHealth;
-    // **CHANGE** private to protected
-    protected int currentHealth; 
+    protected int currentHealth;
 
-    // Constructor (no change needed unless your base class is different)
+    // --- NEW: Static List to Track All Characters ---
+    private static List<Character> allCharacters = new ArrayList<>();
+
+    // --- Constructor (Modified) ---
     public Character(String name, int maxHealth) {
         this.name = name;
         this.maxHealth = maxHealth;
         this.currentHealth = maxHealth;
-        // ... (other initialization)
+        // Add the newly created character to the static list
+        allCharacters.add(this); 
+    }
+    
+    // --- NEW: Static printStatus Method ---
+    public static String printStatus() {
+        StringBuilder status = new StringBuilder();
+        
+        status.append("------------------------------------------\n");
+        status.append("Characters currently fighting : \n");
+
+        for (Character character : allCharacters) {
+            // Append the result of the character's toString method
+            status.append(" - ").append(character.toString()).append("\n");
+        }
+        
+        status.append("------------------------------------------\n");
+        
+        return status.toString();
     }
 
-    // You may need a setter for currentHealth if you don't change it to protected,
-    // but protected is more direct for this problem.
-    // ... (other methods like fight, printStatus, etc.)
+    // --- You will also need a basic toString() in Character for non-Sorcerer instances ---
+    @Override
+    public String toString() {
+        // This format is used for simple Characters like "Frodon : 16/20"
+        return this.name + " : " + this.currentHealth + "/" + this.maxHealth;
+    }
+    
+    // --- Other methods like fight(Character c1, Character c2) go here ---
+    // ...
 }
